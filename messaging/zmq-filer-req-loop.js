ee2f0f9692d89d221925a46478ed95ff161c1c70
Mitja Kleider <mitja@kleider.name>
2014-06-29 17:04:10 +0200

    Chapter 4

---
+++ messaging/zmq-filer-req-loop.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const
+	zmq = require('zmq'),
+	filename = process.argv[2],
+	// create request endpoint
+	requester = zmq.socket('req');
+
+// handle replies from responder
+requester.on("message", function(data) {
+	let response = JSON.parse(data);
+	console.log("Received response:", response);
+});
+requester.connect("tcp://localhost:5433");
+// send request for content
+for (let i=1; i<=3; i++) {
+	console.log('Sending request ' + i + ' for ' + filename);
+	requester.send(JSON.stringify({
+		path: filename
+	}));
+}
